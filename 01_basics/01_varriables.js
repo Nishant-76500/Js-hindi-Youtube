cfa0a08d281d50d3483d2d85273dbf3acd590f8f
Nishant-76500 <nishantdeveloper666@gmail.com>
2024-07-31 06:13:12 +0000

    CHANGES

---
+++ 01_basics/01_varriables.js
@@ -0,0 +1,20 @@
+const accountId = 155443
+let accountEmail = "nishant@google.com"
+var accountPassword= "123456"
+let accountState;
+
+accountCity = "shimla"
+
+//accountId = 2  not  allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "nakodar"
+
+console.log(accountId)
+
+console.table([accountEmail,accountId,accountCity,accountState])
+
+/* 
+prefer not to use var because of issue in block scope and functional scope */
+
